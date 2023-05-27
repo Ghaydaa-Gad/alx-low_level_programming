d1cb0ad7deb1990eae3d494331230ca6e93ee475
Ghaydaa-Gad <ghaydaaali06@gmail.com>
2023-05-26 19:47:43 -0700

    Second task

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -1,9 +1,25 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
-* main - check the code
+* _strlen - Calculate the length of a string
+* @s: The input string
 *
-* Return: Always EXIT_SUCCESS.
+* Return: The length of the string
+*/
+int _strlen(const char *s)
+{
+int length = 0;
+while (s[length] != '\0')
+{
+length++;
+}
+return (length);
+}
+/**
+* main - Check the code
+*
+* Return: Always EXIT_SUCCESS
 */
 int main(void)
 {
