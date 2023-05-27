5b7dbce7d833dd2de1ec704ad1d7055a3be2e92f
Ghaydaa-Gad <ghaydaaali06@gmail.com>
2023-05-26 21:47:43 -0700

    Advanced tasks

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,55 @@
+/**
+* add - Adds two integers.
+* @a: The first integer.
+* @b: The second integer.
+*
+* Return: The sum of the two integers.
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
+/**
+* sub - Subtracts one integer from another.
+* @a: The first integer.
+* @b: The second integer.
+*
+* Return: The difference between the two integers.
+*/
+int sub(int a, int b)
+{
+return (a - b);
+}
+/**
+* mul - Multiplies two integers.
+* @a: The first integer.
+* @b: The second integer.
+*
+* Return: The product of the two integers.
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
+/**
+* div - Divides one integer by another.
+* @a: The numerator.
+* @b: The denominator.
+*
+* Return: The quotient of the division.
+*/
+int div(int a, int b)
+{
+return (a / b);
+}
+/**
+* mod - Computes the remainder of division between two integers.
+* @a: The dividend.
+* @b: The divisor.
+*
+* Return: The remainder of the division.
+*/
+int mod(int a, int b)
+{
+return (a % b);
+}
